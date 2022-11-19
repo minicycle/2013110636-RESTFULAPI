d30802c14e2ae56d498f5237ef9ede1efaf89e7e
minicycle <siratat59@gmail.com>
2022-11-19 11:57:23 +0700

    workshopB

--- AttendanceCheck.md
+++
@@ -1,4 +0,0 @@
->> Thanarat (Ikkyu) : Charpter 0 - Course
-    Introduction & Set up Enviroment
-    Thanarat (Ikkyu) : Chapter 2 - Course 
-    Basic NPM & Basic Modern Javascript
---
+++ workshopB.js
@@ -0,0 +1,8 @@
+const users2 = [
+    {name:'Nite',hobby: 'sleep' ,postcode: 10220},
+    {name:'Nit',hobby: 'eat',postcode: 10221},
+    {name:'Noi',hobby: 'walk',postcode: 10222}
+]
+for(const u of users2){
+    console.log(`My nicname is ${u.name}, My hobby is ${u.hobby} and my postcode is ${u.postcode}`)
+}
